6cfffaf3f1bb6c1598e311388534c4f96100c02c
23mervinwijaya <23mervinwijaya@gmail.com>
2024-02-17 20:22:55 +0700

    indent

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ app.use(expresslayouts);
 app.use(express.static('Public'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(express.json());
+app.use(express.urlencoded());
 
 
-// get method for data from database
-app.get('/',async(req,res)=>{
+// get method for loading all data from employee_data
+app.get('/contact',async(req,res)=>{
     const employeeDatas = await db('data_pegawai').select('*').orderBy('id','asc');
     const employeesData = JSON.stringify(employeeDatas);
     const employeeData = JSON.parse(employeesData);
@@ -33,8 +34,26 @@ app.get('/',async(req,res)=>{
 
 })
 
+// add new data handle
+app.get('/contact/add',async(req,res)=>{
+    
+    res.render('addData',{
+        title : 'Add New Employee Data',
+        layout : './layouts/main-layout'
+    })
+})
+app.post('/contact',async(req,res)=>{
+    const {nama_pegawai,job_level,tanggal_lahir} = req.body;
+    const employeeData =  await db('data_pegawai').insert({
+        nama_pegawai : nama_pegawai,
+        job_level : job_level,
+        tanggal_lahir : tanggal_lahir,
+    }).returning(['id']);
+    res.redirect('/contact')
+})
+
 // detail page of Employee Data
-app.get('/:id',async(req,res)=>{
+app.get('/contact/:id',async(req,res)=>{
     const id = req.params.id;
     console.log(id)
     const employeeDatas = await db('data_pegawai').select('*').where('id',id).first();
@@ -51,7 +70,10 @@ app.get('/:id',async(req,res)=>{
 })
 
 
-app.use('/',(req,res)=>{
+
+
+
+app.use('/',async(req,res)=>{
     res.status = 404;
     res.send('404 Page Not Found')
 })
